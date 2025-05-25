84b0e80e2648987999e3539dce1fe7ff31701849
magomed.ankhaev <anhaev2bk.ru>
2025-05-25 16:12:01 +0300

    homework 9

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +18,47 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
+func Serialize(person any) string {
 	// need to implement
-	return ""
+	if person == nil {
+		return ""
+	}
+
+	t := reflect.TypeOf(person)
+	v := reflect.ValueOf(person)
+
+	for v.Kind() == reflect.Ptr {
+		t = t.Elem()
+		v = v.Elem()
+	}
+
+	if t.Kind() != reflect.Struct {
+		return ""
+	}
+
+	str := strings.Builder{}
+	for i := range t.NumField() {
+		f := t.Field(i)
+
+		tag := f.Tag.Get("properties")
+		tags := strings.Split(tag, ",")
+		if len(tags) == 0 {
+			tags = append(tags, strings.ToLower(f.Name))
+		}
+
+		omitempty := false
+		if len(tags) > 1 && tags[1] == "omitempty" {
+			omitempty = true
+		}
+
+		if omitempty && v.Field(i).IsZero() {
+			continue
+		}
+
+		str.WriteString(fmt.Sprintf("%s=%v\n", tags[0], v.Field(i).Interface()))
+	}
+
+	return str.String()[:len(str.String()) - 1]
 }
 
 func TestSerialization(t *testing.T) {
